d2d6b986ea254c1876dc294a8dd58ab052ea7e03
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-13 10:39:37 +0200

    Gjort en indexOf

--- src/App.java
+++ src/App.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
-        System.out.println("Skriv en text: ");
+        System.out.print("Skriv en text: ");
 
         String text = tb.nextLine();
 
+        System.out.println('"'+text+'"');
         System.out.println("Längden på texten är: " + text.length());
 
+        System.out.println("Tecknet a ligger på plats: "+text.indexOf("a")+" i texten");
+        
        
     }
 }

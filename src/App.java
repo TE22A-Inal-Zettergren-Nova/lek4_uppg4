88d8f2bbf97715b1701049b1d171f319d53ae919
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-20 10:03:57 +0200

    Gjort klart uppgift E med replace metoden

--- src/App.java
+++ src/App.java
@@ -3,30 +3,43 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
-        System.out.println("Uppgift A");
+         //* Uppgift A*/
         System.out.print("Skriv en text: ");
 
         String text = tb.nextLine();
 
         System.out.println('"'+text+'"');
         System.out.println("Längden på texten är: " + text.length());
-
-        System.out.println("Uppgift B");
+        
+         //* Uppgift B*/
         System.out.println("Tecknet a ligger på plats: "+text.indexOf("a")+" i texten");
         
-        System.out.println("Uppgift C");
-        System.out.print("Skriv en siffra där texten börjar: ");
+         //* Uppgift C*/
+        System.out.print("Skriv en siffra där texten ska delas: ");
         int start = tb.nextInt();
-        System.out.print("Skriv en siffra där texten slutar: ");
+        System.out.print("Skriv en till siffra där texten ska delas: ");
         int slut = tb.nextInt();
 
         String del_sträng = text.substring(start, slut);
 
         System.out.println(del_sträng);
-
-        System.out.println("Uppgift D");
-
-        System.out.println("Din text innehåller Hej: " + text.contains("Hej"));
        
+        //* Uppgift D*/
+        System.out.println("Din text innehåller Hej: " + text.contains("Hej"));
+        
+        //* Uppgift E*/
+
+        tb.nextLine();
+        System.out.println("Välj en del av din text som ska bytas ut: ");
+        String byta = tb.nextLine();
+
+        System.out.println("Skriv vad delen ska bytas ut till: ");
+        String byta2 = tb.nextLine();
+
+        System.out.println(text.replace(byta, byta2));
+
+
+        
+
     }
 }
